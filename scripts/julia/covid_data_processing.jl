493f1982e6904044a2a6d4d2be0d02671447ffc4
Chris Boyls-White <23140364+problemxsolutions@users.noreply.github.com>
2021-01-08 14:16:07 -0800

    initial commit covid_data_processing.jl

---
+++ scripts/julia/covid_data_processing.jl
@@ -0,0 +1,170 @@
+# System: Linux 5.4.0-40-generic, Ubuntu 20.04
+# Julia: Version 1.4.1 (2020-04-14)
+
+# COVID Tracking Project
+# URL: https://covidtracking.com/data/api
+
+# World Population Review: US States
+# URL: https://worldpopulationreview.com/states
+
+using HTTP
+using CSVFiles
+using DataFrames
+using Dates
+using Statistics
+using Missings
+using Chain
+
+
+
+base_url = "https://api.covidtracking.com"
+all_states = "/v1/states/daily.csv"
+# current_states = "/v1/states/current.csv"
+
+# state_specific_api = "/v1/states/{state}/daily.csv"
+# current_state = "/v1/states/{state}/current.csv"
+base_dir = "/home/linux/ProblemXSolutions.com/DataProjects/covid19"
+filename = "/data/all_states_daily_covid_jl.csv"
+
+download(string(base_url, all_states), string(base_dir, filename));
+
+# ******************************************************************************
+# Read in the data
+
+state_data = load(string(base_dir, filename)) |> DataFrame
+state_pop_data = load(string(base_dir,"/data/state_populations.csv")) |> DataFrame
+state_name_lookup_data = load(string(base_dir, "/data/state_lu.csv")) |> DataFrame
+
+# # ******************************************************************************
+# Data Processing
+
+state_pop_reduced =
+    @chain state_pop_data begin
+        select(([:State, :Pop, :density]))
+        rename(Dict(:State => :state))
+        transform(:state => (x -> strip.(x)) => :state)
+    end
+
+state_pop_data = Nothing
+
+state_name_lookup_data.state = strip.(state_name_lookup_data.state)
+
+# state_pop_reduced,
+state_pop_reduced =
+    @chain state_pop_reduced begin
+        leftjoin(
+            state_name_lookup_data,
+            on =:state)
+        select(Not([:state]))
+        rename(Dict(:state_abr => :state))
+    end
+# select!(state_pop_reduced, Not([:state]))
+# rename!(state_pop_reduced, Dict(:state_abr => :state))
+
+# # Cleaning the column of values I'm using to conduct the join to enusure a clean
+# # join of values
+# state_data.state = strip.(state_data.state)
+
+# Since Julia is sensitive when applying a function to a field with missing
+# valus, I will remove those data points.  I can safely do this because I know
+# the values being removed are teritories and districts that I am not
+# processing.  There are processes you can do to work with missing values but
+# I dont need to implement for this effort
+dropmissing!(state_pop_reduced)
+state_pop_reduced.state = strip.(state_pop_reduced.state)
+
+# state_data_enhanced =
+#     leftjoin(
+#         state_data,
+#         state_pop_reduced,
+#         on =:state
+#     )
+
+state_data_enhanced =
+    @chain state_data begin
+        transform(:state => (x -> strip.(x)) => :state,
+        :date => (x -> Date.(string.(x), Dates.DateFormat("yyyymmdd"))) => :date)
+        leftjoin(state_pop_reduced,
+            on =:state)
+        sort([:state,:date])
+    end
+
+state_data = Nothing
+
+# state_data_enhanced[:date] = Date.(string.(state_data_enhanced.date), Dates.DateFormat("yyyymmdd"))
+# sort!(state_data_enhanced, [:state, :date])
+
+# Adjusting for population to help normalize numbers between states.
+# (daily_{cases,deaths} / population of state) * 100,000
+
+# Define functions
+function deltas(v, k)
+    [ 1 <= i-k <= length(v) ? v[i]-v[i-k] : 0 for i=1:length(v) ]
+end
+
+function pop_adjusted(x, y)
+    [(x / y) * 100000]
+end
+
+function moving_average(v,k)
+    [ 1 <= i-k <= length(v) ? sum(@view v[(i-k+1):i])/k : 0 for i in 1:length(v) ]
+end
+
+# replace!(state_data_enhanced.recovered, missing => 0)
+# transform!(groupby(state_data_enhanced, :state), [:positiveIncrease, :Pop] => ByRow(pop_adjusted) => :daily_cases_adj)
+# transform!(groupby(state_data_enhanced, :state), [:daily_recover, :Pop] => ByRow(pop_adjusted) => :daily_recover_adj)
+# transform!(groupby(state_data_enhanced, :state), [:deathIncrease, :Pop] => ByRow(pop_adjusted) => :daily_deaths_adj)
+# transform!(groupby(state_data_enhanced, :state), :positiveIncrease => (x -> moving_average(x, 7)) => :active_roll7)
+# transform!(groupby(state_data_enhanced, :state), :daily_recover => (x -> moving_average(x, 7)) => :recovered_roll7)
+# transform!(groupby(state_data_enhanced, :state), :deathIncrease => (x -> moving_average(x, 7)) => :deaths_roll7)
+# transform!(groupby(state_data_enhanced, :state), :daily_cases_adj => (x -> moving_average(x, 7)) => :active_roll7_adj)
+# transform!(groupby(state_data_enhanced, :state), :daily_recover_adj => (x -> moving_average(x, 7)) => :recovered_roll7_adj)
+# transform!(groupby(state_data_enhanced, :state), :daily_deaths_adj => (x -> moving_average(x, 7)) => :deaths_roll7_adj)
+
+# transform!(groupby(state_data_enhanced, :state), :recovered => (x -> deltas(x, 1)) => :daily_recover)
+#
+# transform!(groupby(state_data_enhanced, :state),
+#     [:positiveIncrease, :Pop] => ByRow(pop_adjusted) => :daily_cases_adj,
+#     [:daily_recover, :Pop] => ByRow(pop_adjusted) => :daily_recover_adj,
+#     [:deathIncrease, :Pop] => ByRow(pop_adjusted) => :daily_deaths_adj)
+#
+# transform!(groupby(state_data_enhanced, :state),
+#     :positiveIncrease => (x -> moving_average(x, 7)) => :active_roll7,
+#     :daily_recover => (x -> moving_average(x, 7)) => :recovered_roll7,
+#     :deathIncrease => (x -> moving_average(x, 7)) => :deaths_roll7,
+#     :daily_cases_adj => (x -> moving_average(x, 7)) => :active_roll7_adj,
+#     :daily_recover_adj => (x -> moving_average(x, 7)) => :recovered_roll7_adj,
+#     :daily_deaths_adj => (x -> moving_average(x, 7)) => :deaths_roll7_adj)
+
+state_data_enhanced =
+    @chain state_data_enhanced begin
+        groupby(:state)
+        transform(:recovered => (x -> deltas(x, 1)) => :daily_recover)
+        transform(
+            [:positiveIncrease, :Pop] => ByRow(pop_adjusted) => :daily_cases_adj,
+            [:daily_recover, :Pop] => ByRow(pop_adjusted) => :daily_recover_adj,
+            [:deathIncrease, :Pop] => ByRow(pop_adjusted) => :daily_deaths_adj)
+        transform(
+            :positiveIncrease => (x -> moving_average(x, 7)) => :active_roll7,
+            :daily_recover => (x -> moving_average(x, 7)) => :recovered_roll7,
+            :deathIncrease => (x -> moving_average(x, 7)) => :deaths_roll7,
+            :daily_cases_adj => (x -> moving_average(x, 7)) => :active_roll7_adj,
+            :daily_recover_adj => (x -> moving_average(x, 7)) => :recovered_roll7_adj,
+            :daily_deaths_adj => (x -> moving_average(x, 7)) => :deaths_roll7_adj)
+    end
+
+# ******************************************************************************
+state_enhanced_reduced =
+    @chain state_data_enhanced begin
+        select(
+            :date, :state,
+            :positive, :recovered, :death,
+            :positiveIncrease, :daily_cases_adj, :active_roll7, :active_roll7_adj,
+            :daily_recover, :daily_recover_adj, :recovered_roll7, :recovered_roll7_adj,
+            :deathIncrease, :daily_deaths_adj, :deaths_roll7, :deaths_roll7_adj,
+            :hospitalizedIncrease, :hospitalizedCurrently, :inIcuCurrently
+        )
+    end
+
+save(string(base_dir, "/data/state_data_enhanced_jl.csv"), state_data_enhanced)
+save(string(base_dir, "/data/state_enhanced_reduced_jl.csv"), state_enhanced_reduced)
